7ad5ae3be153f26dadf23531e83ab29d82f8f776
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-09-26 23:56:59 -0400

    Renamed FXML file to "prodLineTracker.fxml"

--- src/prodLineTracker/Main.java
+++ src/prodLineTracker/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
 
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("prodLineTracker.fxml"));
     primaryStage.setTitle("Production Line Tracker");
     primaryStage.setScene(new Scene(root, 657, 470));
     root.getStylesheets().add("prodLineTracker/prodLineTracker.css");
