bd1fcf465857a0296d3c122f3d1d9b004cafc2ce
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-07 16:40:52 -0500

    Screen Class: Added JavaDoc comments to the Constructor and the methods.

--- src/productlinetracker/Screen.java
+++ src/productlinetracker/Screen.java
@@ -12,44 +12,93 @@ package productlinetracker;
 public class Screen implements ScreenSpec {
 
   private String resolution;
-  private int refreshrate;
-  private int responsetime;
+  private int refreshRate;
+  private int responseTime;
 
-  public Screen(String resolution, int refreshrate, int responsetime) {
+  /**
+   * Constructor to use when creating a Screen object.
+   *
+   * @param resolution The Screen's resolution stored as a String variable.
+   * @param refreshRate The Screen's refresh rate stored as an int variable.
+   * @param responseTime The Screen's response time stored as an int variable.
+   */
+  Screen(String resolution, int refreshRate, int responseTime) {
     this.resolution = resolution;
-    this.refreshrate = refreshrate;
-    this.responsetime = responsetime;
+    this.refreshRate = refreshRate;
+    this.responseTime = responseTime;
   }
 
+  /**
+   * Accessor for resolution
+   *
+   * @return resolution, the private String field.
+   */
   public String getResolution() {
     return resolution;
   }
 
+  /**
+   * Accessor for refreshRate
+   *
+   * @return refreshRate, the private int field.
+   */
   public int getRefreshRate() {
 
-    return refreshrate;
+    return refreshRate;
   }
 
+  /**
+   * Accessor for responseTime
+   *
+   * @return responseTime, the private int field.
+   */
   public int getResponseTime() {
 
-    return responsetime;
+    return responseTime;
   }
 
-  public void setResoulution(String resolution) {
+  /**
+   * Mutator for resolution
+   *
+   * @param resolution, the String variable that the private String field is assigned to.
+   */
+  @SuppressWarnings("unused")
+  public void setResolution(String resolution) {
     this.resolution = resolution;
   }
 
-  public void setRefreshRate(int refreshrate) {
-    this.refreshrate = refreshrate;
+  /**
+   * Mutator for refreshRate
+   *
+   * @param refreshRate, the int variable that the private String field is assigned to.
+   */
+  @SuppressWarnings("unused")
+  public void setRefreshRate(int refreshRate) {
+    this.refreshRate = refreshRate;
   }
 
-  public void setResponseTime(int responsetime) {
-    this.responsetime = responsetime;
+  /**
+   * Mutator for responseTime
+   *
+   * @param responseTime, the int variable that the private String field is assigned to.
+   */
+  @SuppressWarnings("unused")
+  public void setResponseTime(int responseTime) {
+    this.responseTime = responseTime;
   }
 
+  /**
+   * Method that returns the Screen's information as a String
+   *
+   * @return resolution, which is a private String variable, refreshRate, which is a private int
+   *     variable, and responseTime, which is a private int variable.
+   */
   public String toString() {
-    return "Resolution: " + getResolution() +
-        "\nRefresh Rate: " + getRefreshRate() +
-        "\nResponse Time: " + getResponseTime();
+    return "Resolution: "
+        + getResolution()
+        + "\nRefresh Rate: "
+        + getRefreshRate()
+        + "\nResponse Time: "
+        + getResponseTime();
   }
 }
