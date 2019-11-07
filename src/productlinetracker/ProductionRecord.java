33ce887b9f31227d63c638bd07d481c4a5d12563
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-07 12:44:58 -0500

    ProductionRecord class: Added JavaDoc comments to each Constructor and Method, and added SuppressWarnings for unused methods.

--- src/productlinetracker/ProductionRecord.java
+++ src/productlinetracker/ProductionRecord.java
@@ -21,8 +21,9 @@ public class ProductionRecord {
   private Date dateProduced;
 
   /**
-   * Constructor called when user records production from the user interface
-   * @param productID
+   * Constructor called when user records production from the user interface.
+   *
+   * @param productID the int variable that the productID field is assigned to.
    */
   public ProductionRecord(int productID) {
     this.productID = productID;
@@ -31,59 +32,127 @@ public class ProductionRecord {
     this.dateProduced = new Date();
   }
 
-  public ProductionRecord(int productionNumber, int productID, String serialNumber,
-      Date dateProduced) {
+  /**
+   * Constructor used for creating ProductionRecord objects that are stored to the database.
+   *
+   * @param productionNumber the int variable that the productionNumber field is assigned to.
+   * @param productID the int variable that the productID field is assigned to.
+   * @param serialNumber the String variable that the serialNumber field is assigned to.
+   * @param dateProduced the date variable that the dateProduced field is assigned to.
+   */
+  public ProductionRecord(
+      int productionNumber, int productID, String serialNumber, Date dateProduced) {
     this.productionNumber = productionNumber;
     this.productID = productID;
     this.serialNumber = serialNumber;
-    //caller makes to the original dateProduced object
     this.dateProduced = dateProduced;
   }
 
+  /**
+   * Constructor that generates a unique serial number for each product produced.
+   *
+   * @param aProduct a Product object that obtained the manufacturer and type for the serial number.
+   * @param itemCount an int variable that is used to be incremented for every product produced.
+   */
   public ProductionRecord(Product aProduct, int itemCount) {
-    this.serialNumber = aProduct.getManufacturer().substring(0,3) + aProduct.getType().getCode() +
-        String.format("%05d", itemCount);
+    this.serialNumber =
+        aProduct.getManufacturer().substring(0, 3)
+            + aProduct.getType().getCode()
+            + String.format("%05d", itemCount);
     this.productionNumber = 0;
     this.productID = 0;
     this.dateProduced = new Date();
   }
 
+  /**
+   * Method that returns the Product information in the form of a string.
+   *
+   * @return the product's productionNumber, productID, serialNumber, and dateProduced.
+   */
   public String toString() {
-    return "Production Num: " + productionNumber +
-        " Product ID: " + productID +
-        " Serial Num: " + serialNumber + " Date: " + dateProduced;
+    return "Production Num: "
+        + productionNumber
+        + " Product ID: "
+        + productID
+        + " Serial Num: "
+        + serialNumber
+        + " Date: "
+        + dateProduced;
   }
 
+  /**
+   * Accessor for productionNumber
+   *
+   * @return productionNumber, the private String field.
+   */
+  @SuppressWarnings("unused")
   public int getProductionNumber() {
     return productionNumber;
   }
 
+  /**
+   * Mutator for productionNumber
+   *
+   * @param productionNumber, the String variable that the private String field is assigned to.
+   */
+  @SuppressWarnings("unused")
   public void setProductionNumber(int productionNumber) {
     this.productionNumber = productionNumber;
   }
 
-  public int getProductID() {
+  /**
+   * Accessor for productID
+   *
+   * @return productID, the private String field.
+   */
+  int getProductID() {
     return productID;
   }
 
-  public void setProductID(int productID) {
+  /**
+   * Mutator for productID
+   *
+   * @param productID, the int variable that the private String field is assigned to.
+   */
+  void setProductID(int productID) {
     this.productID = productID;
   }
 
-  public String getSerialNumber() {
+  /**
+   * Accessor for serialNumber
+   *
+   * @return serialNumber, the private String field.
+   */
+  String getSerialNumber() {
     return serialNumber;
   }
 
+  /**
+   * Mutator for serialNumber
+   *
+   * @param serialNumber, the String variable that the private String field is assigned to.
+   */
+  @SuppressWarnings("unused")
   public void setSerialNumber(String serialNumber) {
     this.serialNumber = serialNumber;
   }
 
-  public Date getDateProduced() {
+  /**
+   * Accessor for dateProduced
+   *
+   * @return dateProduced, the private String field.
+   */
+  Date getDateProduced() {
     return dateProduced;
   }
 
+  /**
+   * Mutator for dateProduced
+   *
+   * @param dateProduced, the Date variable that the private String field is assigned to.
+   */
+  @SuppressWarnings("unused")
   public void setDateProduced(Date dateProduced) {
     this.dateProduced = dateProduced;
   }
-
 }
