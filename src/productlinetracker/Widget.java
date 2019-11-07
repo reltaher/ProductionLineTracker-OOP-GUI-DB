ce4308b1a5b92bae2522eab600f2cb51a5e86a52
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-07 16:47:23 -0500

    Widget Class: Formatted code and added a JavaDoc comment to the Constructor.

--- src/productlinetracker/Widget.java
+++ src/productlinetracker/Widget.java
@@ -11,8 +11,15 @@ package productlinetracker;
  */
 class Widget extends Product {
 
-  Widget(String name, ItemType type, String manufacturer)
-  {
+  /**
+   * Constructor which is used to create Product objects in the form of a Widget. The constructor
+   * inherits the fields from the Product class, and uses those fields to create objects.
+   *
+   * @param name The Product's name.
+   * @param type The Product's type.
+   * @param manufacturer The Product's manufacturer.
+   */
+  Widget(String name, ItemType type, String manufacturer) {
     super(name, type, manufacturer);
   }
 }
