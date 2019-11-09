952d999dbcca4391d9b86eb908e4cbbcf60a6bc5
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-09 17:17:14 -0500

    Updated JavaDoc information in the ScreenSpec interface.

--- src/productlinetracker/ScreenSpec.java
+++ src/productlinetracker/ScreenSpec.java
@@ -4,10 +4,10 @@ package productlinetracker;
  * Represents the author of the program.
  *
  * @author Ramzy El-Taher
- *     The program is a software made in JavaFX where it tracks the number and types of products
- *     being made. This file is the Main class, where the program is launched and executed. The
- *     program is checked with CheckStyle using Google Checks, which uses the p tag.
- *     Date: 10/14/19
+ *     <p>The program is a software made in JavaFX where it tracks the number and types of products
+ *     being made. This file is the ScreenSpec interface, where a Product's screen resolution,
+ *     refresh rate, and response time are obtained.
+ *     <p>Date: 10/14/19
  */
 public interface ScreenSpec {
   // Each method is implicitly public, so the access modifier is not needed.
