7fb1224df8e60735fa412742dfb1cab59806f479
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-09 17:16:07 -0500

    Updated JavaDoc information in the Screen class.

--- src/productlinetracker/Screen.java
+++ src/productlinetracker/Screen.java
@@ -4,10 +4,9 @@ package productlinetracker;
  * Represents the author of the program.
  *
  * @author Ramzy El-Taher
- *     The program is a software made in JavaFX where it tracks the number and types of products
- *     being made. This file is the Main class, where the program is launched and executed. The
- *     program is checked with CheckStyle using Google Checks, which uses the p tag.
- *     Date: 11/04/19
+ *     <p>The program is a software made in JavaFX where it tracks the number and types of products
+ *     being made. This file is the Screen class, where the Product's screen information is made.
+ *     <p>Date: 11/04/19
  */
 public class Screen implements ScreenSpec {
 
