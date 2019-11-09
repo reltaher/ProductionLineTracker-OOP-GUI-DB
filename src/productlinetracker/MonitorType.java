ad9f5f839d5643eab65d52c5f64b607d3071f4ee
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-09 17:18:10 -0500

    Updated JavaDoc information in the MonitorType enum.

--- src/productlinetracker/MonitorType.java
+++ src/productlinetracker/MonitorType.java
@@ -4,13 +4,11 @@ package productlinetracker;
  * Represents the author of the program.
  *
  * @author Ramzy El-Taher
- *
- *     The program is a software made in JavaFX where it tracks the number and types of products
- *     being made. This file is the Main class, where the program is launched and executed. The
- *     program is checked with CheckStyle using Google Checks, which uses the p tag.
- *
- *     Date: 10/14/19
+ *     <p>The program is a software made in JavaFX where it tracks the number and types of products
+ *     being made. This file is the MonitorType enum, which defines a Product's monitor type.
+ *     <p>Date: 10/14/19
  */
 public enum MonitorType {
-  LCD, LED
+  LCD,
+  LED
 }
