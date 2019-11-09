96ccdab9f96dde1225a7cb37244b42db9b91240e
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-09 17:11:26 -0500

    Updated JavaDoc information in the Widget class.

--- src/productlinetracker/Widget.java
+++ src/productlinetracker/Widget.java
@@ -4,10 +4,9 @@ package productlinetracker;
  * Represents the author of the program.
  *
  * @author Ramzy El-Taher
- *     The program is a software made in JavaFX where it tracks the number and types of products
- *     being made. This file is the Main class, where the program is launched and executed. The
- *     program is checked with CheckStyle using Google Checks, which uses the p tag.
- *     Date: 10/01/19
+ *     <p>The program is a software made in JavaFX where it tracks the number and types of products
+ *     being made. This file is the Widget Class, which is a class used to create Product objects.
+ *     <p>Date: 10/01/19
  */
 class Widget extends Product {
 
