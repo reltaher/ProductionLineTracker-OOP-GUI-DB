da1e9926a58e51276bf2329451945ee3017ec16e
reltaher <rjeltaher5059@eagle.fgcu.edu>
2019-11-09 17:01:08 -0500

    Fixed an issue with the Date.

--- src/productlinetracker/ProductionRecord.java
+++ src/productlinetracker/ProductionRecord.java
@@ -45,7 +45,7 @@ public class ProductionRecord {
     this.productionNumber = productionNumber;
     this.productID = productID;
     this.serialNumber = serialNumber;
-    this.dateProduced = new Date(dateProduced.getTime());
+    this.dateProduced = dateProduced;
   }
 
   /**
@@ -153,6 +153,6 @@ public class ProductionRecord {
    */
   @SuppressWarnings("unused")
   public void setDateProduced(Date dateProduced) {
-    this.dateProduced = new Date(dateProduced.getTime());
+    this.dateProduced = dateProduced;
   }
 }
